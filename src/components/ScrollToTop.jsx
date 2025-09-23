c442d52e9e7794f4fd2ab60565559433802edd9f
SHIV KUMAR <shivkumar@SHIVs-MacBook-Air.local>
2025-09-23 10:41:57 +0530

    Update project files

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-}
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // smooth scroll to top
+    });
+  }, [pathname]);
+
+  return null; // nothing to render
+};
 
 export default ScrollToTop;
